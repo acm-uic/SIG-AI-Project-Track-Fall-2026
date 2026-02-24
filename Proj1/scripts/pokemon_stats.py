8edb7581846def41304b72fedf8fd2925aca01d7
sammypatel06 <sammypatel06@gmail.com>
2026-02-23 18:00:35 -0600

    Update pokemon_stats.py

--- Proj1/scripts/pokemon_stats.py
+++ Proj1/scripts/pokemon_stats.py
@@ -1,4 +1,7 @@
 import requests
+import pandas as pd
+import pathlib
+
 
 BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
 
@@ -9,20 +12,38 @@ def get_pokemon_stats(pokemon_name):
     if response.status_code == 200:
         data = response.json()
         stats = {stat['stat']['name']: stat['base_stat'] for stat in data['stats']}
-        return stats
+        types = {type['type']['name'] for type in data['types']}
+        return stats, tuple(types)
     
     else:
         print(f"Error: Could not retrieve data for {pokemon_name}. Status code: {response.status_code}")
         return None
-    
 
 if __name__ == "__main__":
 
+    # Data Folder: ../data
+    data_folder = pathlib.Path("../data")
+    if not data_folder.exists():
+        data_folder.mkdir()
+
+
+    # Create dataframe
     pokemon_name = input("Enter the name of the Pokémon (for Hisuian variants, add '-hisui'): ")
 
-    stats = get_pokemon_stats(pokemon_name)
+    stats, types = get_pokemon_stats(pokemon_name)
+
+    stats['type1'] = types[0]
+
+    try:
+        stats['type2'] = types[1]
+    except IndexError:
+        stats['type2'] = None
     
-    if stats:
-        print(f"Stats for {pokemon_name.capitalize()}:")
-        for stat_name, stat_value in stats.items():
-            print(f"{stat_name.capitalize()}: {stat_value}")
+    df = pd.DataFrame(stats, index=[pokemon_name])
+    print(df)
+
+    # Save to CSV
+    csv_path_string = data_folder + "/poke_stats.csv"
+    df.to_csv(csv_path)
+    print(f"{csv_path} saved!")
+    
