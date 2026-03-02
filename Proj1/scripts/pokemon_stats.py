9a32a5993450ab00e77df7b8bf92872a572c04b6
Uzair Azizuddin <azizuddinuzair@gmail.com>
2026-03-02 12:00:38 -0600

    Done

--- Proj1/scripts/pokemon_stats.py
+++ Proj1/scripts/pokemon_stats.py
@@ -5,6 +5,10 @@ import pathlib
 
 BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
 
+
+
+
+
 def get_pokemon_stats(pokemon_name):
     url = f"{BASE_URL}{pokemon_name.lower()}"
     response = requests.get(url)
@@ -39,6 +43,7 @@ if __name__ == "__main__":
     except IndexError:
         stats['type2'] = None
     
+<<<<<<< HEAD
     df = pd.DataFrame(stats, index=[pokemon_name])
     print(df)
 
@@ -47,3 +52,55 @@ if __name__ == "__main__":
     df.to_csv(csv_path)
     print(f"{csv_path} saved!")
     
+=======
+    if stats:
+        print(f"Stats for {pokemon_name.capitalize()}:")
+        for stat_name, stat_value in stats.items():
+            print(f"{stat_name.capitalize()}: {stat_value}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def get_json(url):
+#     return requests.get(url).json()
+
+# def get_fully_evolved_pokemon(limit=1025):
+#     fully_evolved = []
+
+#     for i in range(1, limit + 1):
+#         pokemon = get_json(f"{BASE_URL}/pokemon/{i}")
+#         species = get_json(pokemon["species"]["url"])
+#         evo_chain = get_json(species["evolution_chain"]["url"])
+
+#         def find_in_chain(chain):
+#             if chain["species"]["name"] == pokemon["name"]:
+#                 return chain
+#             for evo in chain["evolves_to"]:
+#                 found = find_in_chain(evo)
+#                 if found:
+#                     return found
+#             return None
+
+#         node = find_in_chain(evo_chain["chain"])
+
+#         if node and len(node["evolves_to"]) == 0:
+#             stats = {stat["stat"]["name"]: stat["base_stat"] for stat in pokemon["stats"]}
+#             fully_evolved.append({
+#                 "name": pokemon["name"],
+#                 **stats
+#             })
+
+#         time.sleep(0.2)  # be polite to API
+
+#     return fully_evolved
+
+>>>>>>> f7a689d (Done)
